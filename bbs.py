cb24db5b68fc745d7ad220cc67b01f8d5dcd14ec
drwadu <alamzan94@live.de>
2019-12-02 16:02:56 +0100

    none

--- bbs.py
+++ bbs.py
@@ -3,7 +3,7 @@ from src.output import output, log_file
 
 def main():
 
-    o = BBS('test_case/test_data_.csv', 5, 10, 13, 15)
+    o = BBS('test_case/testdata.csv', 5, 10, 13, 15)
     print(output(o.compare_singleton_instances(o.infos)))
     #print(output(o.compare_tuple_instances(o.infos, o.data)))
     log_file(o.compare_singleton_instances(o.infos), 'test_case')
